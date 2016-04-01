f0645af585fb0409adc23bebf49970165fd8c7c5
Tyler Green <xylr@live.com>
2016-04-01 10:41:38 -0600

    Updated mainView so that it scales up the art
    Updated mainView so that it scales up the art by a factor of 10

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include "Player.h"
 
 int main(){
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-	std::vector<Entity> entities;
-	
-	initTextures("listOfTextures.json");
-	initPlayer(entities);
+    // Create the main window & set up view to upscale the 64x64 game by a factor of 10.
+    sf::RenderWindow window(sf::VideoMode(640, 640), "SFML window");
+    sf::View mainView(sf::FloatRect(0, 0, 64, 64));
+    window.setView(mainView);
+    std::vector<Entity> entities;
+    initTextures("listOfTextures.json");
+    initPlayer(entities);
     
     while (window.isOpen()){
         
